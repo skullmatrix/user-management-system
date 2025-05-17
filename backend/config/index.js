3945716d05fcf6aaec86952aa8900e6dc4625de0
unknown <YY222@ccs.local>
2025-05-17 20:19:31 +0800

    pushing successful API integration

--- backend/config/index.js
+++ backend/config/index.js
@@ -4,9 +4,9 @@ module.exports = {
     database: {
         host: "153.92.15.31",
         port: 3306,
-        user: "u875409848_go",
+        user: "u875409848_planas",
         password: "9T2Z5$3UKkgSYzE",
-        database: "u875409848_go"
+        database: "u875409848_planas"
     },
     secret: "supersecretcode",
     emailFrom: "info@node-mysql-signup-verification-api.com",
--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'https://user-management-system-979j.onrender.com',
+  baseUrl: 'https://planas-fullstack-user-management.onrender.com',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
