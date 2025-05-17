1f33dc4620c11527635c7b32e2978cb44a09f724
unknown <YY222@ccs.local>
2025-05-17 20:47:41 +0800

    pushing successful API integration

--- backend/_helpers/db.js
+++ backend/_helpers/db.js
@@ -21,9 +21,9 @@ try {
             database: {
                 host: "153.92.15.31",
                 port: 3306,
-                user: "u875409848_planas",
+                user: "u875409848_go",
                 password: "9T2Z5$3UKkgSYzE",
-                database: "u875409848_planas"
+                database: "u875409848_go"
             }
         };
     }
@@ -50,9 +50,9 @@ async function initialize() {
         const { 
             host = "153.92.15.31", 
             port = 3306, 
-            user = "u875409848_planas", 
+            user = "u875409848_go", 
             password = "9T2Z5$3UKkgSYzE", 
-            database = "u875409848_planas" 
+            database = "u875409848_go" 
         } = config.database || {};
         
         console.log('Using database configuration:');
